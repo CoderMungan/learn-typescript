75f50dc06abce11a378a332035ea5e759930395e
CoderMungan <codermungan@gmail.com>
2023-12-07 00:51:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var message = "hello world!";
+console.log(message);
+var h1 = document.createElement('h1');
+h1.textContent = 'Hello World!';
+document.body.appendChild(h1);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let message: string = "hello world!"
+console.log(message);
+
+const h1 = document.createElement('h1')
+h1.textContent = 'Hello World!'
+document.body.appendChild(h1)
